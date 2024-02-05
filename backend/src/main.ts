647e03e81161562579e51411e684a2e98f8a8d48
ybdorego <yann.dorego@limayrac.com>
2024-02-05 13:12:48 +0100

    CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  // app.enableCors();
+
+  app.enableCors({
+    origin: 'http://localhost:8000/', // or an array of allowed origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // enable credentials (cookies, authorization headers)
+  });
+
+
   await app.listen(3000);
 }
 bootstrap();
