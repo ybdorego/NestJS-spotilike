78d67cca46408308eadba2d323a374aeeb737a1a
ybdorego <yann.dorego@limayrac.com>
2024-02-06 00:03:23 +0100

    commentaire + correction

--- backend/src/morceau/controllers/morceau.controller.ts
+++ backend/src/morceau/controllers/morceau.controller.ts
@@ -12,16 +12,26 @@ export class MorceauController {
 	}
 
     
-     // get tous les morceau
     @Get()
+    /**
+     * Récupère tous les morceaux.
+     * @returns Une promesse qui se résout avec un tableau de Morceau.
+     */
     async getallMorceau(): Promise<Morceau[]> {
         return this.morceauService.findAll();
     }
 
 
     // creation d'un Morceau
-    @Post(':idArtiste')
+    @Post('CreateArtiste/:idArtiste')
     @UsePipes(new ValidationPipe)
+    /**
+     * Crée un nouvel album.
+     * 
+     * @param idArtiste - L'identifiant de l'artiste.
+     * @param morceau - Les informations du morceau à créer.
+     * @returns Une promesse résolue avec le morceau créé.
+     */
     async createAlbum(
         @Param('idArtiste') 
         idArtiste: string,
@@ -31,8 +41,14 @@ export class MorceauController {
         return this.morceauService.create(idArtiste,morceau)
     }
 
-    // touvée un morceau par son id 
+
     @Get(':id')
+    /**
+     * Récupère un morceau par son identifiant.
+     *
+     * @param id - L'identifiant du morceau.
+     * @returns Une promesse résolue avec le morceau correspondant à l'identifiant.
+     */
     async getMorceauById(
         @Param('id') 
         id: string
@@ -40,8 +56,15 @@ export class MorceauController {
         return this.morceauService.findById(id);
     }
 
-    // mise a jours d'un morceau par son id 
+    
     @Put(':id')
+    /**
+     * Met à jour un morceau.
+     *
+     * @param id - L'identifiant du morceau à mettre à jour.
+     * @param morceau - Les données de mise à jour du morceau.
+     * @returns Une promesse résolue avec le morceau mis à jour.
+     */
     async UpdateMorceau(
         @Param('id') 
         id: string,
@@ -50,4 +73,9 @@ export class MorceauController {
     ): Promise<Morceau> {
         return this.morceauService.updateById(id, morceau)
     }
+
+
+
+
+    
 }
--- backend/src/morceau/morceau.service.ts
+++ backend/src/morceau/morceau.service.ts
@@ -16,6 +16,15 @@ export class MorceauService {
         @InjectModel(Album.name) private albumModel: mongoose.Model<Album>,
         ) {}
         
+        /**
+         * Crée un morceau pour un album donné.
+         *
+         * @param artisteid - L'ID de l'artiste associé au morceau.
+         * @param albumId - L'ID de l'album auquel le morceau appartient.
+         * @param createMorceauDto - Les données du morceau à créer.
+         * @returns Une promesse résolue avec le morceau créé.
+         * @throws {HttpException} Si l'artiste ou l'album n'est pas trouvé.
+         */
         async createMorceauforalbum(artisteid, albumId: mongoose.Types.ObjectId,  createMorceauDto: CreateMorceauDto): Promise<Morceau>{
             const Artiste = await this.artisteModel.findById(artisteid)
         
@@ -40,14 +49,6 @@ export class MorceauService {
                         },
                     });
                 }
-                // Ajoute le morceau à la liste des morceaux de l'album
-                if (!Album.morceaux.includes(morceau.id)) {
-                    // await Album.updateOne({
-                    //     $push: {
-                    //         morceaux: morceau.id,
-                    //     },
-                    // });
-                }
             } else {
                 // Si le morceau n'existe pas, créez un nouveau morceau
                 morceau = new this.morceauModel({...createMorceauDto, artistes: [artisteid]});
@@ -64,43 +65,14 @@ export class MorceauService {
             return morceau;
         }
 
-        // async createMorceauforalbum(artisteid, albumId: mongoose.Types.ObjectId,  createMorceauDto: CreateMorceauDto): Promise<Morceau>{
-        //     const Artiste = await this.artisteModel.findById(artisteid)
-        
-        //     if(!Artiste) throw new HttpException("artiste non trouvé", 404);
-        
-        //     let morceau = await this.morceauModel.findOne({ titre: createMorceauDto.titre });
-        
-        //     if (morceau) {
-        //         // Si le morceau existe déjà, on ajoute l'id de l'artiste au tableau artistes du morceau
-        //         if (!morceau.artistes.includes(artisteid)) {
-        //             morceau.artistes.push(artisteid);
-        //             morceau = await morceau.save();
-        //         }
-        //         // Ajoute le morceau à la liste des morceaux de l'artiste
-        //         if (!Artiste.morceaux.includes(morceau.id)) {
-        //             await Artiste.updateOne({
-        //                 $push: {
-        //                     morceaux: morceau.id,
-        //                 },
-        //             });
-        //         }
-        //     } else {
-        //         // Si le morceau n'existe pas, créez un nouveau morceau
-        //         morceau = new this.morceauModel({...createMorceauDto, artistes: [artisteid]});
-        //         morceau = await morceau.save();
-        
-        //         await Artiste.updateOne({
-        //             $push: {
-        //                 morceaux: morceau.id,
-        //             },
-        //         });
-        //     }
-        
-        //     return morceau;
-        // }
-
-       
+        /**
+         * Crée un morceau pour un artiste donné.
+         *
+         * @param artisteid - L'ID de l'artiste associé au morceau.
+         * @param createMorceauDto - Les données du morceau à créer.
+         * @returns Une promesse résolue avec le morceau créé.
+         * @throws {HttpException} Si l'artiste n'est pas trouvé.
+         */
         async create(artisteid,  createMorceauDto: CreateMorceauDto): Promise<Morceau>{
             const Artiste = await this.artisteModel.findById(artisteid)
         
@@ -133,24 +105,39 @@ export class MorceauService {
                     },
                 });
             }
-        
             return morceau;
         }
 
 
-         // afficher tout les morceaus 
+        /**
+         * Récupère tous les morceaux.
+         * @returns Une promesse qui se résout avec un tableau de morceaux.
+         */
          async findAll(): Promise<Morceau[]>{
             const morceaus = await this.morceauModel.find();
             return morceaus;
         }
 
-        // crée un morceau seul 
+        
+        /**
+         * Crée un morceau individuel.
+         *
+         * @param morceau - Les informations du morceau à créer.
+         * @returns Une promesse résolue avec le morceau créé.
+         */
         async createMorceauAlone(morceau: Morceau): Promise<Morceau>{
             const res = await this.morceauModel.create(morceau)
             return res;
         }
 
-        // trouvé un morceau par son id
+        
+        /**
+         * Recherche un morceau par son identifiant.
+         * @param id L'identifiant du morceau à rechercher.
+         * @returns Une promesse résolue avec le morceau correspondant à l'identifiant.
+         * @throws BadRequestException si l'identifiant n'est pas valide.
+         * @throws NotFoundException si aucun morceau n'est trouvé avec l'identifiant donné.
+         */
         async findById(id: string): Promise<Morceau>{
 
             const isvalid = mongoose.isValidObjectId(id)
@@ -166,7 +153,12 @@ export class MorceauService {
             return morceau;
         }
 
-        // trouvé un morceau par son nom
+        /**
+         * Récupère un morceau par son nom.
+         * @param name Le nom du morceau à rechercher.
+         * @returns Une promesse résolue avec le morceau correspondant au nom.
+         * @throws NotFoundException si aucun morceau n'est trouvé avec le nom donné.
+         */
         async findMorceauByName(name: string): Promise<Morceau> {
             const morceau = await this.morceauModel.findOne({ titre: name });
         
@@ -177,20 +169,49 @@ export class MorceauService {
             return morceau;
         }
 
-        // maj de l'morceau par son id
-        async updateById(id: string, morceau: UpdateMorceauDto): Promise<Morceau>{
-            return await this.morceauModel.findByIdAndUpdate(id,morceau, {
-                     new: true,
-                     runValidators :true
-             });    
-        }
+        
+        /**
+         * Met à jour un morceau en utilisant son identifiant.
+         *
+         * @param id - L'identifiant du morceau à mettre à jour.
+         * @param morceau - Les données de mise à jour du morceau.
+         * @returns Une promesse qui se résout avec le morceau mis à jour.
+         */
+        // async updateById(id: string, morceau: UpdateMorceauDto): Promise<Morceau>{
+        //     return await this.morceauModel.findByIdAndUpdate(id,morceau, {
+        //              new: true,
+        //              runValidators :true
+        //      });    
+        // }
 
-        // Supprime un morceau
+        async updateById(id: string, morceau: UpdateMorceauDto): Promise<Morceau> {
+            const existinMorceau = await this.morceauModel.findById(id);
+            if (!existinMorceau) {
+                    throw new NotFoundException('morceau non trouvé');
+            }
+
+            // Mettre à jour les propriétés du morceau existant
+            Object.assign(existinMorceau, morceau);
+
+            // Sauvegarder le morceau mis à jour
+            const updatedMorceau = await existinMorceau.save();
+
+            return updatedMorceau;
+    }
+
+        /**
+         * Supprime un morceau, en le retirant de l'album et de l'artiste associés.
+         *
+         * @param id - L'identifiant du morceau à supprimer.
+         * @returns Une promesse qui se résout lorsque le morceau est supprimé avec succès.
+         * @throws {BadRequestException} Si l'identifiant n'est pas valide.
+         * @throws {NotFoundException} Si le morceau n'est pas trouvé.
+         */
         async deleteById(id: string): Promise<void> {
-            const isvalid = mongoose.isValidObjectId(id);
+            const isValid = mongoose.isValidObjectId(id);
 
-            if (!isvalid) {
-                throw new BadRequestException('Entrez un id valide.');
+            if (!isValid) {
+                throw new BadRequestException('Entrez un ID valide.');
             }
 
             const deletedMorceau = await this.morceauModel.findByIdAndDelete(id);
@@ -198,6 +219,18 @@ export class MorceauService {
             if (!deletedMorceau) {
                 throw new NotFoundException('Morceau non trouvé');
             }
+
+            // Remove the song from the associated album by name
+            await this.albumModel.updateOne(
+                { _id: deletedMorceau.album },
+                { $pull: { morceaux: deletedMorceau.titre } }
+            );
+
+            // Remove the song from the associated artist
+            await this.artisteModel.updateOne(
+                { _id: deletedMorceau.artistes },
+                { $pull: { morceaux: deletedMorceau._id } }
+            );
         }
 
 }
