80c48796d72c16c6b6704431603ec9ded95a8acf
ybdorego <yann.dorego@limayrac.com>
2024-02-07 00:04:00 +0100

    test

--- SpotilikeFront/src/app/auth/login/login.component.ts
+++ SpotilikeFront/src/app/auth/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  public usernameFormControl = new FormControl(null, [
+    Validators.required,
+    Validators.email,
+  ]);
+  public passwordFormControl = new FormControl(null, [Validators.minLength(4)]);
+
+  public userForm!: FormGroup;
+
+
+
+  submit() {
+    console.log(this.userForm.value);
+  }
+
 
 }
